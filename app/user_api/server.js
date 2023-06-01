aa439dc1958e4ed685d1d560951a1d950582c68e
he2754le <hendrik.lendeckel@fh-erfurt.de>
2023-06-01 15:11:42 +0200

    swagge instalation

--- app/user_api/server.js
+++ app/user_api/server.js
@@ -3,6 +3,8 @@
 import express from 'express'
 import { router as usersRouter } from "./routes/users.js";
 import mongoose from "mongoose"
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
 
 mongoose.connect("mongodb://database/foodiemate");
 
@@ -16,6 +18,32 @@ const port = 3000
 
 app.use("/users", usersRouter);
 
+
+const options = {
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            title: "Express API with Swagger",
+            version: "0.1.0",
+            description: "This is a simple CRUD API application made with Express and documented with Swagger",
+        },
+    },
+    servers: [
+        {
+            url: 'http://localhost:3000',
+            description: 'Development server',
+        },
+    ],
+    apis: ["./src/**/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
