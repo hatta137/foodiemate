2413a07e2fe9c10f021f12f2aa2f58b39b8de9b0
he2754le <hendrik.lendeckel@fh-erfurt.de>
2023-06-17 22:30:39 +0200

    vit vs crp

--- app/react_frontend/src/App.jsx
+++ app/react_frontend/src/App.jsx
@@ -1,24 +1,34 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Login from "./components/Login.jsx";
-import Register from "./components/Register.jsx";
 
 function App() {
-    const [currentForm, setCurrentForm] = useState('login')
-    //const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    const toggleForm = (formName) => {
-        setCurrentForm(formName)
-    }
+  const [count, setCount] = useState(0)
 
   return (
-      <div className="App">
-          {
-              currentForm === 'login' ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm}/>
-          }
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
